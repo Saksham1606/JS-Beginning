053ba7bfb1a7407e56524b646466d8e0ea089ae4
Saksham1606 <rohanrajputktp@gmail.com>
2024-12-01 12:37:16 +0530

    About Datatypes

---
+++ 01_basics/data.js
@@ -0,0 +1,41 @@
+// So, we have some datatypes in javascript(JS)
+
+// INTEGER DATATYPE:
+Number = 1345
+console.log(Number);
+//it's range ends at 2^15;
+
+//STRING DATATYPE:
+String = "Saksham Rajput"
+console.log(String);
+
+// BIGINT DATATYPE:
+BigInt = 23565149454879797971
+console.log(BigInt);
+// it is used for big values...
+
+// BOOLEAN DATATYPE:
+Boolean = true
+console.log(Boolean);
+// it stores value true or false..
+
+// NULL => standalone value ;
+//undefined => it stores value undefined;
+// symbol => will be used in react;
+
+// object => will talk further about it;
+
+
+
+// NOW, we can type any datatype using let:
+
+let name = "Saksham Rajput"
+let age = 18
+let virgin = true
+let body_count = null
+let faaltu;
+
+console.table([name, age, virgin, body_count, faaltu])
+
+
+// AND ALL SET!!
